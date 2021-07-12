ad921e17cc99e59488e767f263cefa415460aaeb
punkalimo <pashanijaulani@gmail.com>
2021-07-12 11:44:32 +0200

    First commit

---
+++ src/data.js
@@ -0,0 +1,58 @@
+export const projects = [
+  {
+    title: "Scanlab Motor Spares",
+    subtitle: "Web-based advertising platform ",
+    description:
+      "This website deals with all kind of motor spares and audio accessories for the purpose of advertisement, Scanlab motor spares website provides the latest goods and services with accurate current prices all over the world ",
+    image: "./project-01.png",
+  },
+  {
+    title: "Air Pollution in Bangladesh",
+    subtitle: "Information Dynamic website about air pollution in bangladesh",
+    description:
+      "Among the world's megacities of 10 million people or more, the annual average PM2.5 concetration in Dhaka's air in 2019 was 83.3 microgrammes per cubic metre",
+    image: "./project-02.png",
+  },
+  {
+    title: "Node Coin",
+    subtitle: "A Cryptocurreny that uses the blockchain technology",
+    description:
+      "Blockchain is the technology that enables the existence of cryptocurrency (among other things). ... A cryptocurrency is a medium of exchange, such as the US dollar, but is digital and uses encryption techniques to control the creation of monetary units and to verify the transfer of funds",
+    image: "./project-3.jpg",
+  },
+  {
+    title: "Portfolio Website",
+    subtitle: "The Domain Guy Portfolio Web App ",
+    description:
+      "A portfolio website provides professional information about an individual or a company and presents a showcase of their work, To supply site visitors with information about the individual or company",
+    image: "./project-4.png",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "The Web based advertising platform works very well, all the functional and non functional requirements were well developed to an exceptional degree.",
+    image: "./user.svg",
+    name: "Frank",
+    company: "Scanlab Motor Spares",
+  },
+  {
+    quote:
+      "The Backend of Nodecoin does what was exactly stated in the project proposal.",
+    image: "./user.svg",
+    name: "Muhammad Fayaz",
+    company: "Self employed",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "PHP",
+  "HTML",
+  "Node",
+  "MySQL",
+  "CCNA",
+  "Software Engineering",
+  "Web Development",
+];
