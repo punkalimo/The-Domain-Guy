6917ade3e5154e53ea529c4a4f002bc720bca431
punkalimo <pashanijaulani@gmail.com>
2022-06-09 10:39:55 +0200

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -20,8 +20,8 @@ export default function Contact() {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", name, email, message }),
     })
-      .then(() => alert("Message sent!"))
-      .catch((error) => alert(error));
+    .then(() => alert("Message sent!"))
+    .catch((error) => alert(error));
   }
 
   return (
